399e48a31352406e4e58f031a1a32d65370a5cd1
Ellen Maas <ellen.diana.van.lutse.maas@emory.edu>
2023-04-02 23:26:14 -0400

    Modifies Daycent run process command code
    Adds site name to base and eq files. Removes scenario name from LRF eq and base files.

--- Daycent/Daycent_run_controller.R
+++ Daycent/Daycent_run_controller.R
@@ -8,7 +8,7 @@
 # change. 
 ###########################################################################
 # Audit Trail
-# 2/21/2022: Created script.
+# 2/21/2023: Created script.
 ###########################################################################
 
 print("Starting Daycent_run_controller.R")
@@ -22,14 +22,14 @@ source(paste0("Daycent/run_Daycent_setup_",site_name,".R"))
 ## successfully to preserve the output. Each step uses the output of the
 ## step before.
 # --------------- Step 1: Run equilibrium simulation (4000-year spin-up) ---------------
-source("Daycent/run_Daycent_eq.R")
+source(paste0("Daycent/run_Daycent_eq_",site_name,".R"))
 
 # # --------------- Step 2: Run base cropping simulations (land conversion - start exp) ---------------
-source("Daycent/run_Daycent_base.R")
+source(paste0("Daycent/run_Daycent_base_",site_name,".R"))
 # 
 # # --------------- Step 3: Run experimental period simulations  ---------------
-source("Daycent/run_Daycent_exp.R")
+# source("Daycent/run_Daycent_exp.R")
 # 
 # # --------------- Step 4: Run future emissions scenarios (end exp-2100) --------------- 
-source("Daycent/run_Daycent_fut.R")
+# source("Daycent/run_Daycent_fut.R")
 
--- Daycent/run_Daycent_setup_LRF.R
+++ Daycent/run_Daycent_setup_LRF.R
@@ -10,18 +10,10 @@
 # 9/27/2022: Rewrote to more generic time points, removed site name
 # 12/13/2022: Reverted equilibrium run naming to remove scenario number.
 # 1/3/2023: Added soiln.out to output files processing.
+# 4/2/2023: Removed soiln references for LRF.
 ###########################################################################
 
 print("Starting run_Daycent_setup.R")
-
-#***** If running this script independently, uncomment this group 
-# Clear memory before rerunning the script. 
-#rm(list=ls())
-#setwd("~")
-#site_name="KBS"
-# Load observations and global constants
-#source("Modeling/0_Observations_and_constants.R")
-#*****
 
 # Set the model path to the location of this site's files.
 prior_path <- getwd()
@@ -37,28 +29,6 @@ if(Sys.info()['sysname']=='Linux') {
   daycent_executable <- "DD17centEVI.exe"
   daycent_list100 <- "DD17list100.exe"
 }
-
-
-# --------------- Install this scenario's input files ---------------------
-#
-# Load scenario-specific input files.
-# if(mgmt_scenario_grp==2) {
-#   file.copy("soils_2.in","soils.in", overwrite=TRUE)
-#   file.copy("site_2.100","site.100", overwrite=TRUE)
-#   file.copy("fix_2.100","fix.100", overwrite=TRUE)
-# } else if(mgmt_scenario_grp==3) {
-#   file.copy("soils_3.in","soils.in", overwrite=TRUE)
-#   file.copy("site_3.100","site.100", overwrite=TRUE)
-#   file.copy("fix_3.100","fix.100", overwrite=TRUE)
-# } else {
-#   file.copy("soils_1.in","soils.in", overwrite=TRUE)
-#   file.copy("site_1.100","site.100", overwrite=TRUE)
-#   file.copy("fix_1.100","fix.100", overwrite=TRUE)
-# }
-
-# use default crop.100 except for future for scenario 3
-file.copy("crop_allothers.100","crop.100", overwrite=TRUE)
-
 
 # --------------- Reset working directory --------------- 
 setwd(prior_path)
