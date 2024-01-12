ca580ce8519b4bb0f109a5309e8adebb674c0897
adampdixon <adampdixon@umbc.edu>
2024-01-12 09:47:29 -0700

    adding bash tests#

--- sh_sends/bashloop_proto_.sh
+++ sh_sends/bashloop_proto_.sh
@@ -4,13 +4,15 @@ echo "Running 000_County_Run.R in bash parallel"
 
 # Define the task to be executed in parallel
 task () {
-  Rscript bashloop_prototype.R --args $1
+  ml load ncarenv-basic/23.09 gcc gdal geos proj udunits conda rstudio
+  conda activate r-4.3
+  Rscript /glade/derecho/scratch/apdixon/national_gwp/bashloop_prototype_.R --args $1
   sleep 0.5
   echo "$1"
 }
-
+sh
 # Define the number of parallel processes
-NUM_PARALLEL=3
+NUM_PARALLEL=1
 
 # Read the input arguments from a file and run the command in parallel
 for i in $(seq 1 10); do
@@ -20,4 +22,3 @@ for i in $(seq 1 10); do
     wait
   fi
 done
-
