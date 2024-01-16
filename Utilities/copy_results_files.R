fb4b6f9deb169d6a675ed0d18565dfd2d1b7a0c2
adampdixon <adampdixon@umbc.edu>
2024-01-15 20:00:17 -0600

    updated copy files script

--- Utilities/copy_results_files.R
+++ Utilities/copy_results_files.R
@@ -1,7 +1,12 @@
+# Copy files from results folder to folder for transfer via Globus
 
+library(R.utils)
+
+# File name
 r2<-list.files('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=T, pattern='Annual_results', full.names=F)
+# file name and full path
 r1<-list.files('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=T, pattern='Annual_results', full.names=T)
 
 for (i in 1:length(r2)){
-  file.copy(r1[i], paste0('/glade/derecho/scratch/apdixon/national_gwp_results_send/',gsub("/", "_", r2[i]))) 
+  copyFile(r1[i], paste0('/glade/derecho/scratch/apdixon/national_gwp_results_send/',gsub("/", "_", r2[i]))) 
 }
