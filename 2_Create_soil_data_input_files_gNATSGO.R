5aef6f983bf56920f99368d551d06f27548f06c9
adampdixon <adampdixon@umbc.edu>
2024-02-09 09:04:37 -0500

    Update 2_Create_soil_data_input_files_gNATSGO.R

--- 2_Create_soil_data_input_files_gNATSGO.R
+++ 2_Create_soil_data_input_files_gNATSGO.R
@@ -43,65 +43,64 @@ geo_link<-read.csv(file.path(geo_link_dir, 'county_geoid_link.csv'))%>%
   arrange(zh_geoid)%>%
   as_tibble()
 
-county_number<-args[2]
+# county_number<-args[2]
 print(paste0("The county_number is: ", county_number, " \n"))
 
-output_file<-file.path(output_dir, paste('GEOID_', GEOID, '_gNATSGO.csv', sep = ''))''
+output_file<-file.path(output_dir, paste('GEOID_', GEOID, '_gNATSGO.csv', sep = ''))
 
-if(!file.exists(output_file)){
-  
-  # GEOID, value
-  
-  # parameters<-c('Bulk Density', 'Clay', 'pH', 'Sand', 'Silt', 'SOC')
-  depths<-c('0 to 2', '2 to 5', '5 to 10', '10 to 20', '20 to 30', 
-            '30 to 45', '45 to 60', '60 to 75', '75 to 90', '90 to 105',
-            '105 to 120', '120 to 150', '150 to 180', '180 to 200')
-  
-  
-  
-  
-  # GEOID, Depth, BD, Clay, pH, Sand, Silt, SOC
-  
-  county_data<-data.frame()
-  
-  for(i in 1:length(depths)){
+# parameters<-c('Bulk Density', 'Clay', 'pH', 'Sand', 'Silt', 'SOC')
+depths<-c('0 to 2', '2 to 5', '5 to 10', '10 to 20', '20 to 30', 
+          '30 to 45', '45 to 60', '60 to 75', '75 to 90', '90 to 105',
+          '105 to 120', '120 to 150', '150 to 180', '180 to 200')
+
+for (i in 0:3108){
+  if(!file.exists(output_file)){
     
-    zh_GEOID<-filter(geo_link, zh_geoid==county_number)$zh_geoid
-    GEOID<-filter(geo_link, zh_geoid==county_number)$REAL_GEOID
-    Depth<-depths[i]
+    # GEOID, value
+    # GEOID, Depth, BD, Clay, pH, Sand, Silt, SOC
     
-    # print(Depth)
+    county_data<-data.frame()
     
-    # Get paramter csvs lined up
-    BD<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('Bulk Density', Depth, sep = ' ')))
-    Clay<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('Clay', Depth, sep = ' ')))
-    pH<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('pH', Depth, sep = ' ')))
-    Sand<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('Sand', Depth, sep = ' ')))
-    Silt<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('Silt', Depth, sep = ' ')))
-    SOC<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('SOC', Depth, sep = ' ')))
+    for(i in 1:length(depths)){
+      
+      zh_GEOID<-filter(geo_link, zh_geoid==county_number)$zh_geoid
+      GEOID<-filter(geo_link, zh_geoid==county_number)$REAL_GEOID
+      Depth<-depths[i]
+      
+      # print(Depth)
+      
+      # Get paramter csvs lined up
+      BD<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('Bulk Density', Depth, sep = ' ')))
+      Clay<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('Clay', Depth, sep = ' ')))
+      pH<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('pH', Depth, sep = ' ')))
+      Sand<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('Sand', Depth, sep = ' ')))
+      Silt<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('Silt', Depth, sep = ' ')))
+      SOC<-fread(list.files(gnatsgo_dir, full.names = T, pattern = paste('SOC', Depth, sep = ' ')))
+      
+      # read and filter each parameters values
+      BD2<-filter(BD, GEOID==zh_GEOID)[,2][[1]]
+      Clay2<-filter(Clay, GEOID==zh_GEOID)[,2][[1]]
+      pH2<-filter(pH, GEOID==zh_GEOID)[,2][[1]]
+      Sand2<-filter(Sand, GEOID==zh_GEOID)[,2][[1]]
+      Silt2<-filter(Silt, GEOID==zh_GEOID)[,2][[1]]
+      SOC2<-filter(SOC, GEOID==zh_GEOID)[,2][[1]]
+      
+      
+      # bind data
+      
+      data<-cbind(GEOID, Depth, BD2, Clay2, pH2, Sand2, Silt2, SOC2)
+      
+      county_data<-rbind(county_data, data)
+      
+    }
     
-    # read and filter each parameters values
-    BD2<-filter(BD, GEOID==zh_GEOID)[,2][[1]]
-    Clay2<-filter(Clay, GEOID==zh_GEOID)[,2][[1]]
-    pH2<-filter(pH, GEOID==zh_GEOID)[,2][[1]]
-    Sand2<-filter(Sand, GEOID==zh_GEOID)[,2][[1]]
-    Silt2<-filter(Silt, GEOID==zh_GEOID)[,2][[1]]
-    SOC2<-filter(SOC, GEOID==zh_GEOID)[,2][[1]]
+    names(county_data)<-c('GEOID', 'Depth_cm', 'BD', 'Clay', 'pH', 'Sand', 'Silt', 'SOC')
     
-    
-    # bind data
-    
-    data<-cbind(GEOID, Depth, BD2, Clay2, pH2, Sand2, Silt2, SOC2)
-    
-    county_data<-rbind(county_data, data)
-    
+    fwrite(county_data, )
+  } else {
+    print(paste0('file already exists', county_number))
   }
   
-  names(county_data)<-c('GEOID', 'Depth_cm', 'BD', 'Clay', 'pH', 'Sand', 'Silt', 'SOC')
-  
-  fwrite(county_data, )
-} else {
-  print(paste0('file already exists', county_number))
 }
 
 
