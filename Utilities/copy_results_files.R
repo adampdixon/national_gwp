4045c79efca3970c1c85edd7c3a9b4de034ff7bc
adampdixon <adampdixon@umbc.edu>
2024-03-04 00:09:11 -0500

    Update copy_results_files.R

--- Utilities/copy_results_files.R
+++ Utilities/copy_results_files.R
@@ -7,12 +7,25 @@ library(R.utils)
 # # file name and full path
 # r1<-list.files('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=T, pattern='Annual_results', full.names=T)
 
+# # Dir name
+# r2<-dir('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=T, pattern='Kansas', full.names=F)
+# # Dor name and full path
+# r1<-dir('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=T, pattern='Kansas', full.names=T)
+# 
+# for (i in 1:length(r2)){
+#   paste0('Copying ', gsub("/", "_", r2[i]))
+#   copyFile(r1[i], paste0('/glade/derecho/scratch/apdixon/national_gwp_results_send/',gsub("/", "_", r2[i])), skip=TRUE) 
+# }
+
+
+
+
 # Dir name
-r2<-dir('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=T, pattern='Kansas', full.names=F)
-# Dor name and full path
-r1<-dir('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=T, pattern='Kansas', full.names=T)
+r2<-dir('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=F, pattern='Kansas', full.names=F)
+# Dir name and full path
+r1<-dir('/glade/derecho/scratch/apdixon/national_gwp_results', recursive=F, pattern='Kansas', full.names=T)
 
 for (i in 1:length(r2)){
-  paste0('Copying ', gsub("/", "_", r2[i]))
-  copyFile(r1[i], paste0('/glade/derecho/scratch/apdixon/national_gwp_results_send/',gsub("/", "_", r2[i])), skip=TRUE) 
+  print(paste0('Copying ', gsub("/", "_", r2[i])))
+  copyDirectory(r1[i], paste0('/glade/derecho/scratch/apdixon/national_gwp_results_send/',gsub("/", "_", r2[i])), skip=TRUE) 
 }
