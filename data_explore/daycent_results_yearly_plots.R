75b0a9252d4648327d4dd83e95377c71827c2e6d
adampdixon <adampdixon@umbc.edu>
2024-03-15 00:08:11 -0500

    update plotting and climate bash

--- data_explore/daycent_results_yearly_plots.R
+++ data_explore/daycent_results_yearly_plots.R
@@ -40,6 +40,9 @@ if (Sys.info()['sysname'] == "Linux"){
 library(dplyr)
 library(ggplot2)
 library(reshape2) 
+library(gridExtra)
+library(grid)
+# library(patchwork)
 
 
 # line plots of crops and scenarios
@@ -59,11 +62,11 @@ r1<-dir(results_path, recursive=F, full.names=T)
 
 # create plots for all crops
 
-for (c in c('Maize', 'Soybeans', 'Wheat', 'Cotton', 'Rotation')) {
+for (c in c('Maize', 'Soybean', 'Wheat', 'Cotton', 'Rotation')) {
   
   crop_scenario_df<-data.frame()
   for (s in 1:6){
-    print(paste0('working on ', c, ' scenario ', s))
+    print(paste0('working on ', c, ' scenario ', s, ' table'))
     
     # county_n<-0 # counter
     
@@ -77,62 +80,121 @@ for (c in c('Maize', 'Soybeans', 'Wheat', 'Cotton', 'Rotation')) {
         county_string_split<-strsplit(county_string, '_')
         GEOID<-county_string_split[[1]][3]
         State<-county_string_split[[1]][4]
+        
+        if(identical('Rotation', c)){ # deal with Rotation having maize and soybean yield by pulling both columns in select below
+          select_var = c('Maize', 'Soybean')
+        } else {
+          select_var = c
+        }
 
         data<-fread(f)%>%
           mutate(GEOID=GEOID, State=State)%>%
-          select(GEOID, State, year, Scenario, MaizeYld_Mgha, SOC_Mgha, N2OEmissions_ghayr, CH4Emissions_ghayr) #TODO add CO2
+          select(GEOID, State, year, Scenario, paste0(select_var, 'Yld_Mgha'), SOC_Mgha, N2OEmissions_ghayr, CH4Emissions_ghayr) #TODO add CO2
+        
+        if(identical('Rotation', c)){ # populate new column called 'RotationYld_Mgha' depending on which one is NA
+          data$RotationYld_Mgha <- ifelse(is.na(data$MaizeYld_Mgha), data$SoybeanYld_Mgha, data$MaizeYld_Mgha)
+        }
         
         data$Scenario<-s
-        
-        
-      }
-      crop_scenario_df<-rbind(crop_scenario_df, data)
-    } # end of for loop listing files
-  } # end of for loop reading files
+        crop_scenario_df<-rbind(crop_scenario_df, data)
+      } # end of for loop listing files
       
-      # Plot 4 x 6
-      
-      gfg_plot <- function(df, result){
-        p<-ggplot(df, aes(x = year,
-                          y = eval(result),
-                          color = GEOID)) +  
-          geom_line(show.legend = FALSE) +
-          stat_summary(geom="line", fun = "mean", color="black", linewidth=.5) +
-          geom_vline(xintercept=2022, color = 'gray20', linetype="dashed") +
-          scale_x_continuous(breaks = seq(1850, 2050, by = 10)) +
-          theme_classic() +
-          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
-        
-        return(p)
-        
-      }
-      
-      # MaizeYld_Mgha, SOC_Mgha, N2OEmissions_ghayr, CH4Emissions_ghayr
-      
-      scen_1_yield<-gfg_plot(df = scenario_df, result = paste0(c, 'Yld_Mgha'))
-      scen_1_SOC<-gfg_plot(df = scenario_df, result = 'SOC_Mgha')
-      scen_1_N20<-gfg_plot(df = scenario_df, result = 'N2OEmissions_ghayr')
-      scen_1_CH4<-gfg_plot(df = scenario_df, result = 'CH4Emissions_ghayr')
-      
-      
-      
-      
-      out<-arrangeGrob(scen_1_1, scen_1_2, 
-                       scen_2_1, scen_2_2, 
-                       scen_3_1, scen_3_2, 
-                       scen_4_1, scen_4_2,
-                       scen_5_1, scen_5_2,
-                       scen_6_1, scen_6_2,
-                       ncol = 2, nrow = 6)
-      
-      
-      crop_out<-file.path(Output, paste0(Crop_, "_Yield_SOC.png"))
-      
-      ggsave(file = crop_out, plot=out, dpi=300, width = 10, height = 16)
-      
-      
-      
+      } # end of for loop reading files
+    
+    } # end of for loop scenarios
+  
+  
+  # labs(title = yield_title,
+  #      subtitle = paste0(text, " scenario")) +
+  
+  # Plot 4 x 6
+  gfg_plot <- function(df, result){
+    p<-ggplot(df, aes(x = year,
+                      y = get(result),
+                      color = GEOID)) +
+      geom_line(show.legend = FALSE) +
+      stat_summary(geom="line", fun = "mean", color="black", linewidth=.5) +
+      geom_vline(xintercept=2022, color = 'gray20', linetype="dashed") +
+      scale_x_continuous(breaks = seq(1850, 2050, by = 10)) +
+      xlab('') +
+      ylab(result) +
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
+    return(p)
+  }
+  
+  print('plotting')
+  
+  # MaizeYld_Mgha, SOC_Mgha, N2OEmissions_ghayr, CH4Emissions_ghayr
+  
+  crop_scenario_df
+  
+  
+  s1<-filter(crop_scenario_df, Scenario == 1)
+  s2<-filter(crop_scenario_df, Scenario == 2)
+  s3<-filter(crop_scenario_df, Scenario == 3)
+  s4<-filter(crop_scenario_df, Scenario == 4)
+  s5<-filter(crop_scenario_df, Scenario == 5)
+  s6<-filter(crop_scenario_df, Scenario == 6)
+  
+  scen_1_yield<-gfg_plot(df = s1, result = paste0(c, 'Yld_Mgha'))
+  scen_2_yield<-gfg_plot(df = s2, result = paste0(c, 'Yld_Mgha'))
+  scen_3_yield<-gfg_plot(df = s3, result = paste0(c, 'Yld_Mgha'))
+  scen_4_yield<-gfg_plot(df = s4, result = paste0(c, 'Yld_Mgha'))
+  scen_5_yield<-gfg_plot(df = s5, result = paste0(c, 'Yld_Mgha'))
+  scen_6_yield<-gfg_plot(df = s6, result = paste0(c, 'Yld_Mgha'))
+  
+  scen_1_SOC<-gfg_plot(df = s1, result = 'SOC_Mgha')
+  scen_2_SOC<-gfg_plot(df = s2, result = 'SOC_Mgha')
+  scen_3_SOC<-gfg_plot(df = s3, result = 'SOC_Mgha')
+  scen_4_SOC<-gfg_plot(df = s4, result = 'SOC_Mgha')
+  scen_5_SOC<-gfg_plot(df = s5, result = 'SOC_Mgha')
+  scen_6_SOC<-gfg_plot(df = s6, result = 'SOC_Mgha')
+  
+  scen_1_N2O<-gfg_plot(df = s1, result = 'N2OEmissions_ghayr')
+  scen_2_N2O<-gfg_plot(df = s2, result = 'N2OEmissions_ghayr')
+  scen_3_N2O<-gfg_plot(df = s3, result = 'N2OEmissions_ghayr')
+  scen_4_N2O<-gfg_plot(df = s4, result = 'N2OEmissions_ghayr')
+  scen_5_N2O<-gfg_plot(df = s5, result = 'N2OEmissions_ghayr')
+  scen_6_N2O<-gfg_plot(df = s6, result = 'N2OEmissions_ghayr')
+  
+  scen_1_CH4<-gfg_plot(df = s1, result = 'CH4Emissions_ghayr')
+  scen_2_CH4<-gfg_plot(df = s2, result = 'CH4Emissions_ghayr')
+  scen_3_CH4<-gfg_plot(df = s3, result = 'CH4Emissions_ghayr')
+  scen_4_CH4<-gfg_plot(df = s4, result = 'CH4Emissions_ghayr')
+  scen_5_CH4<-gfg_plot(df = s5, result = 'CH4Emissions_ghayr')
+  scen_6_CH4<-gfg_plot(df = s6, result = 'CH4Emissions_ghayr')
+  
+  title<-paste0(c, " Daycent Low Change Climate Scenario")
+  
+  # out<-arrangeGrob(scen_1_yield, scen_1_SOC, scen_1_N2O, scen_1_CH4,
+  #                  scen_2_yield, scen_2_SOC, scen_2_N2O, scen_2_CH4,
+  #                  scen_3_yield, scen_3_SOC, scen_3_N2O, scen_3_CH4,
+  #                  scen_4_yield, scen_4_SOC, scen_4_N2O, scen_4_CH4,
+  #                  scen_5_yield, scen_5_SOC, scen_5_N2O, scen_5_CH4,
+  #                  scen_6_yield, scen_6_SOC, scen_6_N2O, scen_6_CH4,
+  #                  ncol = 4, nrow = 6,
+  #                  top = textGrob(title,gp=gpar(fontsize=20,font=3)))
+  
+  left_justify<-.025
+  
+  out<-grid.arrange(arrangeGrob(scen_1_yield, scen_1_SOC, scen_1_N2O, scen_1_CH4, top = grid::textGrob("Monocropping", x = left_justify, hjust = 0), ncol = 4),
+                    arrangeGrob(scen_2_yield, scen_2_SOC, scen_2_N2O, scen_2_CH4, top = grid::textGrob("No-till", x = left_justify, hjust = 0), ncol = 4),
+                    arrangeGrob(scen_3_yield, scen_3_SOC, scen_3_N2O, scen_3_CH4, top = grid::textGrob("Cover Crop Mix", x = left_justify, hjust = 0), ncol = 4),
+                    arrangeGrob(scen_4_yield, scen_4_SOC, scen_4_N2O, scen_4_CH4, top = grid::textGrob("Cover Crop Cereal", x = left_justify, hjust = 0), ncol = 4),
+                    arrangeGrob(scen_5_yield, scen_5_SOC, scen_5_N2O, scen_5_CH4, top = grid::textGrob("Cover Crop Legume", x = left_justify, hjust = 0), ncol = 4),
+                    arrangeGrob(scen_6_yield, scen_6_SOC, scen_6_N2O, scen_6_CH4, top = grid::textGrob("No-till and Cover Crop", x = left_justify, hjust = 0), ncol = 4),
+                   nrow = 6,
+                   top = textGrob(title,gp=gpar(fontsize=20,font=3)))
+  
+  
+  
+  crop_out<-file.path(output_figs, paste0(c,  "_results.png"))
+  
+  ggsave(file = crop_out, plot=out, dpi=300, width = 16, height = 16)
+  
+  print('done')
   
 }
 
--- sh_sends/bash_climate.sh
+++ sh_sends/bash_climate.sh
@@ -7,13 +7,13 @@
 #PBS -e /glade/derecho/scratch/apdixon/national_gwp/sh_sends/climate.stderr 
 ### Queue name
 #PBS -q main
-#PBS -l walltime=0:20:00
+#PBS -l walltime=4:00:00
 ### Send email on abort, begin and end
 #PBS -m e
 ### Specify mail recipient
 #PBS -M apdixon@pm.me
 ### Number of nodes
-#PBS -l select=1:ncpus=5:mem=20GB
+#PBS -l select=1:ncpus=50:mem=100GB
 ###PBS -l select=1:ncpus=32:mem=235GB
 ########################
 
@@ -36,10 +36,10 @@ task () {
 }
 
 # Define the number of parallel processes
-NUM_PARALLEL=20
+NUM_PARALLEL=50
 
 # Read the input arguments from a file and run the command in parallel
-for i in $(seq 1 20); do
+for i in $(seq 1 3108); do
   echo "Starting task $i"
   task $i &
   if (( $i % $NUM_PARALLEL == 0 )); then
