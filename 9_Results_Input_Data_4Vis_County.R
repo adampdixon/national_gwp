9c371022b8a68721d0aeafd886bd1b168b56d772
adampdixon <adampdixon@umbc.edu>
2024-03-27 23:58:46 -0500

    Update 9_Results_Input_Data_4Vis_County.R

--- 9_Results_Input_Data_4Vis_County.R
+++ 9_Results_Input_Data_4Vis_County.R
@@ -46,7 +46,7 @@ county_data<-read.csv(file.path(master_path, 'Data', 'County_start', 'county_cen
 
 if(identical(Test, TRUE)){
   county_data<-county_data%>%
-    filter(GEOID %in% c(31181)) #13023, 13213, 20073, 31181, 42053, 1075
+    filter(GEOID %in% c(31181, 13023, 13213, 20073, 31181, 42053, 1075))
 }
 
 setwd(master_path)
@@ -65,86 +65,86 @@ county_climate_low_change<-data.frame()
 county_climate_high_change<-data.frame()
 
 
-tryCatch(
-  expr = {
+
     # Get average value per year for each county
     # put table together
-    for (county_geoid in county_data$GEOID){
-      print(county_geoid)
-      
-      state<-filter(county_data, GEOID==county_geoid)$State_Name
-      
-      for (fut_climate in c(1:2)){
-        print(fut_climate)
-        
-        if (fut_climate==1){ # LOW CHANGE
-          # create climate tables
-          source('1_create_county_climate_wth_file_County.R', local = TRUE)
+for (county_geoid in county_data$GEOID){
+      tryCatch(
+        expr = {
+          print(county_geoid)
           
-          # summarize at yearly level
-          weather2<-weather%>%
-            group_by(year)%>%
-            summarise_at(vars(tmax, tmin, precip), mean)
+          state<-filter(county_data, GEOID==county_geoid)$State_Name
           
-          add_df<-data.frame(GEOID = county_geoid, State = state)
+          for (fut_climate in c(1:2)){
+            print(fut_climate)
+            
+            if (fut_climate==1){ # LOW CHANGE
+              # create climate tables
+              source('1_create_county_climate_wth_file_County.R', local = TRUE)
+              
+              # summarize at yearly level
+              weather2<-weather%>%
+                group_by(year)%>%
+                summarise_at(vars(tmax, tmin, precip), mean)
+              
+              add_df<-data.frame(GEOID = county_geoid, State = state)
+              
+              weather3<- cbind(add_df, weather2)
+              
+              # weather is outout of source file
+              county_climate_low_change<-rbind(county_climate_low_change, weather3)
+              
+            } 
+            
+            if (fut_climate==2){ # HIGH CHANGE
+              # create climate tables
+              source('1_create_county_climate_wth_file_County.R', local = TRUE)
+              
+              # summarize at yearly level
+              weather2<-weather%>%
+                group_by(year)%>%
+                summarise_at(vars(tmax, tmin, precip), mean)
+              
+              add_df<-data.frame(GEOID = county_geoid, State = state)
+              
+              weather3<- cbind(add_df, weather2)
+              
+              # weather is outout of source file
+              county_climate_high_change<-rbind(county_climate_high_change, weather3)
+            }
+            
+          }
           
-          weather3<- cbind(add_df, weather2)
+        },
+        error = function(e){ 
           
-          # weather is outout of source file
-          county_climate_low_change<-rbind(county_climate_low_change, weather3)
+          weather2<-data.frame(GEOID = county_geoid, State =  NA, year =NA, day = NA, month = NA, jday = NA, tmax = NA, tmin = NA, precip = NA)
+          county_climate_high_change<-rbind(county_climate_high_change, weather2)
           
-        } 
-        
-        if (fut_climate==2){ # HIGH CHANGE
-          # create climate tables
-          source('1_create_county_climate_wth_file_County.R', local = TRUE)
+          county_climate_low_change<-rbind(county_climate_low_change, weather2)
           
-          # summarize at yearly level
-          weather2<-weather%>%
-            group_by(year)%>%
-            summarise_at(vars(tmax, tmin, precip), mean)
           
-          add_df<-data.frame(GEOID = county_geoid, State = state)
-          
-          weather3<- cbind(add_df, weather2)
-          
-          # weather is outout of source file
-          county_climate_high_change<-rbind(county_climate_high_change, weather3)
+        },
+        warning = function(w){
+          # (Optional)
+          # Do this if a warning is caught...
+        },
+        finally = {
+          # (Optional)
+          # Do this at the end before quitting the tryCatch structure...
         }
-        
-      }
+      )
       
     }
-    
-  },
-  error = function(e){ 
-    
-    weather2<-data.frame(GEOID = county_geoid, State =  NA, year =NA, day = NA, month = NA, jday = NA, tmax = NA, tmin = NA, precip = NA)
-    county_climate_high_change<-rbind(county_climate_high_change, weather2)
-    
-    county_climate_low_change<-rbind(county_climate_low_change, weather2)
-    
-    
-  },
-  warning = function(w){
-    # (Optional)
-    # Do this if a warning is caught...
-  },
-  finally = {
-    # (Optional)
-    # Do this at the end before quitting the tryCatch structure...
-  }
-)
 
 
-fwrite(county_climate_low_change, file.path(results_path, 'Data', 'County_start', 'county_climate_low_change.csv'))
+fwrite(county_climate_low_change, file.path(results_path, 'county_climate_low_change.csv'))
 
-fwrite(county_climate_high_change, file.path(results_path, 'Data', 'County_start', 'county_climate_high_change.csv'))
+fwrite(county_climate_high_change, file.path(results_path, 'county_climate_high_change.csv'))
 
 
 
 # create histogram
-
 
 # create box plots
 
