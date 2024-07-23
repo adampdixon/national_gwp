9ef240acafb628d22f8f6d7241e239e7335fe42d
adampdixon <adampdixon@umbc.edu>
2024-07-23 10:18:59 -0500

    Update copy_input_files_to_transfer.R

--- Utilities/copy_input_files_to_transfer.R
+++ Utilities/copy_input_files_to_transfer.R
@@ -25,11 +25,11 @@ geoids_to_fetch<-c(5007, 4009, 5021, 4015)
 geoids_to_fetch<-paste(geoids_to_fetch, collapse = '_|_')
 geoids_to_fetch<-paste0('_', geoids_to_fetch, '_')
 
-soil_data<-list.files(soils_data_path, full.names = T)
+soil_data<-list.files(soil_data_path, full.names = T)
 climate_data<-list.files(climate_data_path, full.names = T)
 
 # Get soils data over
-file.copy(soils_data[grepl(geoids_to_fetch, soils_data)], glade_soils_send)
+file.copy(soil_data[grepl(geoids_to_fetch, soil_data)], glade_soils_send)
 
 # Get climate data over
 file.copy(climate_data[grepl(geoids_to_fetch, climate_data)], glade_climate_send)
